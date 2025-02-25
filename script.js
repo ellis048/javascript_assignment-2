bf8d0d22f9bca8c49d58a544c92c29cd0e9d2a59
ellis048 <ellis.megacoolest09@outlook.com>
2025-02-25 01:26:11 -0600

    final commit

--- script.js
+++ script.js
@@ -11,7 +11,35 @@ function changeContent() {
 function toggleImageSize(img) {
     if (img.style.width === "350px" ){
         img.style.width = "750px"
+        img.style.border ="5px solid red";
     } else {
         img.style.width = "350px"
+        img.style.border ="none";
+    }
+}
+
+
+//the cursor
+document.addEventListener("mousemove", function(event) {
+    let circle = document.getElementById("cursor-circle");
+    circle.style.left = `${event.clientX}px`;
+    circle.style.top = `${event.clientY}px`;
+    circle.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
+});
+
+//The age checker
+function checkAge() {
+    const yourname = document.getElementById("name").value;
+    const ageInput = document.getElementById("age").value;
+    const feedback = document.getElementById("feedback");
+
+    if (ageInput === "") {
+        feedback.textContent = "";
+    } else if (ageInput < 18) {
+        feedback.textContent = yourname + " you are a minor.";
+        feedback.style.color = "red";
+    } else {
+        feedback.textContent = yourname + " you are an adult.";
+        feedback.style.color = "green";
     }
 }
